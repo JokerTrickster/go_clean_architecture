51946c4dbe7054338a63b9eac124a4faf1a1f991
RyanBreathings <ryan@breathings.co.kr>
2023-09-13 23:06:57 +0900

    {feat} - 프레임워크, 라이브러리 설치
    what : 프레임워크, 라이브러리 설치
    how : swagger, air, echo 설치

--- src/main.go
+++ src/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("github action test")
-	return
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":3000"))
 }

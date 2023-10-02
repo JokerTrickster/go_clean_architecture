1b882ba9da3ffe14f5f53b7712075bff5cf65e7f
RyanBreathings <ryan@breathings.co.kr>
2023-10-02 18:08:07 +0900

    {feat} - aws ssm 기능 구현
    what : aws ssm 기능 구현
    why : 보안상으로 보이면 안되는 값들을 보관하고 사용하기 위해서 생성
    how : us-east-1 리전 사용

---
+++ src/common/aws/cssm/init.go
@@ -0,0 +1,66 @@
+package cssm
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	AwsConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"main/common"
+	"strings"
+)
+
+var AwsClientSsm *ssm.Client
+
+func InitAwsSsm() error {
+	var awsConfig aws.Config
+	var err error
+
+	awsConfig, err = AwsConfig.LoadDefaultConfig(context.TODO(),
+		AwsConfig.WithRegion("us-east-1"))
+	if err != nil {
+		return fmt.Errorf("init aws - region : %s", "us-east-1")
+	}
+
+	AwsClientSsm = ssm.NewFromConfig(awsConfig)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func AwsGetParam(path string) (string, error) {
+	ctx := context.TODO()
+	param, err := AwsClientSsm.GetParameter(ctx, &ssm.GetParameterInput{
+		Name:           aws.String(path),
+		WithDecryption: common.BoolToPointer(true),
+	})
+	if err != nil {
+		return "", fmt.Errorf("get cssm param : %s, %v", path, err)
+	}
+	return aws.ToString(param.Parameter.Value), nil
+}
+
+func AwsGetParams(paths []string) ([]string, error) {
+	ctx := context.TODO()
+	params, err := AwsClientSsm.GetParameters(ctx, &ssm.GetParametersInput{
+		Names:          paths,
+		WithDecryption: common.BoolToPointer(true),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("get cssm params : %s", strings.Join(paths, ","))
+	}
+	result := make([]string, len(paths))
+	for i, path := range paths {
+		val := ""
+		for _, parameter := range params.Parameters {
+			if strings.Contains(aws.ToString(parameter.ARN), path) {
+				val = aws.ToString(parameter.Value)
+				break
+			}
+		}
+		result[i] = val
+	}
+	return result, nil
+}
---
+++ src/common/env.go
@@ -0,0 +1,5 @@
+package common
+
+func BoolToPointer(b bool) *bool {
+	return &b
+}
